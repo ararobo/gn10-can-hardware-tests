c37ffbad609e454cf66cabfe6247483c8a0bf771
Kamagata0 <aroe0725@gmail.com>
2026-02-18 16:19:52 +0900

    #1 format

--- GN10Mainboard/Core/Src/main.c
+++ GN10Mainboard/Core/Src/main.c
@@ -27,28 +27,27 @@
 #include "app/app.hpp"
 #include "stdbool.h"
 /* USER CODE END Includes */
- 
- FDCAN_TxHeaderTypeDef TxHeader;
-  uint32_t TxMailbox;
-  uint16_t TxData[8];
-  
+
+FDCAN_TxHeaderTypeDef TxHeader;
+uint32_t TxMailbox;
+uint16_t TxData[8];
+
 void InitCAN()
 {
 
-  if(HAL_FDCAN_Start(&hfdcan1) != HAL_OK)
+  if (HAL_FDCAN_Start(&hfdcan1) != HAL_OK)
   {
     Error_Handler();
   }
-
 }
 
-bool sendPacket(uint16_t can_id,uint8_t *tx_buffer,uint8_t data_length)
+bool sendPacket(uint16_t can_id, uint8_t *tx_buffer, uint8_t data_length)
 {
-  if(data_length > 8)
+  if (data_length > 8)
   {
-      return 0;
+    return 0;
   }
-  
+
   TxHeader.Identifier = can_id;
   TxHeader.IdType = FDCAN_STANDARD_ID;
   TxHeader.TxFrameType = FDCAN_DATA_FRAME;
@@ -59,18 +58,18 @@ bool sendPacket(uint16_t can_id,uint8_t *tx_buffer,uint8_t data_length)
   TxHeader.TxEventFifoControl = FDCAN_NO_TX_EVENTS;
   TxHeader.MessageMarker = 0;
 
-  if(0 < HAL_FDCAN_GetTxFifoFreeLevel(&hfdcan1))
-{
-   if(HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader,tx_buffer) != HAL_OK)
-   {
+  if (0 < HAL_FDCAN_GetTxFifoFreeLevel(&hfdcan1))
+  {
+    if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, tx_buffer) != HAL_OK)
+    {
       return false;
-   }
-}
+    }
+  }
 
-else
-{
-  return false;
-}
+  else
+  {
+    return false;
+  }
   return true;
 }
 
@@ -95,7 +94,6 @@ else
 
 /* USER CODE END PV */
 
-
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
@@ -108,9 +106,9 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -147,29 +145,29 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    uint8_t tx_data= 1;
-    sendPacket(0x00,tx_data,sizeof(tx_data));
+    uint8_t tx_data = 1;
+    sendPacket(0x00, tx_data, sizeof(tx_data));
     HAL_Delay(1000);
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -185,9 +183,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -204,9 +201,9 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -219,12 +216,12 @@ void Error_Handler(void)
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
